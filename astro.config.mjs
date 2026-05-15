a4ea41c5a2a9cc234ac9f033f4a80b93f91bc5e8
JesusAlmeda <55757734+JesusAlmeda03@users.noreply.github.com>
2026-05-15 12:39:51 -0600

    deploy version 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
+export default defineConfig({
+  base: '/landing-astro/',
+});
+
 // https://astro.build/config
-export default defineConfig({});
+//export default defineConfig({});
